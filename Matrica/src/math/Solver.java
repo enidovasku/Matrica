37bbfe68e64cf87f83894df8cca46c24c6ba1077
Mateo Jovani <mateojovani@yahoo.com>
2015-10-26 19:47:14 +0100

    -reworked solving

--- Matrica/src/math/Solver.java
+++ Matrica/src/math/Solver.java
@@ -2,10 +2,13 @@ package math;
 
 public class Solver
 {
+	private boolean stop = false;//stop from incrementing phase
+	
 	private double delta;
 	private double x1;
 	private double x2;
 	private double x3;
+	private String unexpectedResult;
 	
 	public double getX1()
 	{
@@ -26,43 +29,74 @@ public class Solver
 	{
 		return this.delta;
 	}
+	
+	//gjeneron matricen e fazes se radhes
+	public Matrice generateNextMatrice(Matrice inputMatrice, int phase)
+	{
+		if(inputMatrice.type == false)
+		{
+			if(phase == 1)
+			{				
+				if((inputMatrice.a1 == 0 && inputMatrice.b1 == 0)||(inputMatrice.a2 == 0 && inputMatrice.b2 == 0)) 
+					this.unexpectedResult = "Ky sistem ekuacionesh nuk ka zgjidhje";
+				
+				if(inputMatrice.a1 == 0) this.x2 = inputMatrice.a3/inputMatrice.a2; 
+				else
+				{
+				  //multiply by some factor to make the key 1
+				  inputMatrice.a1 = inputMatrice.a1*(1/inputMatrice.a1);
+				  inputMatrice.a2 = inputMatrice.a2*(1/inputMatrice.a1);
+				  inputMatrice.a3 = inputMatrice.a3*(1/inputMatrice.a1);
+				}
+				if(inputMatrice.b1 == 0) this.x2 = inputMatrice.b3/inputMatrice.b2;
+				else
+				{	
+				  inputMatrice.b1 = inputMatrice.b1*(1/inputMatrice.b1);
+				  inputMatrice.b2 = inputMatrice.b2*(1/inputMatrice.b1);
+				  inputMatrice.b3 = inputMatrice.b3*(1/inputMatrice.b1);
+				}
+			}
+			
+			else if(phase == 2)
+			{
+				//substract
+				inputMatrice.b1 = inputMatrice.b1 - inputMatrice.a1;	
+				inputMatrice.b2 = inputMatrice.b2 - inputMatrice.a2;	
+				inputMatrice.b3 = inputMatrice.b3 - inputMatrice.a3;		
+			}
+			
+			else if(phase == 3)
+			{
+				//substract
+				inputMatrice.b2 = inputMatrice.b2*(1/inputMatrice.b2);	
+				inputMatrice.b3 = inputMatrice.b3*(1/inputMatrice.b2);		
+			}
+			
+			else if(phase == 4)
+			{
+				double mul = inputMatrice.a2/inputMatrice.b2;
+				inputMatrice.a2 = 0;
+				inputMatrice.a3 = inputMatrice.a3 - mul*inputMatrice.b3;
+				this.x1 = inputMatrice.a3;
+				this.x2 = inputMatrice.b3;
+				this.stop = true;
+			}
+			phase++;
+		}
+		
+		else
+		{
+			//3x3 matrice <---- HERE
+		}
+		
+		
+		if(this.stop == true) return inputMatrice;	
+		else return generateNextMatrice(inputMatrice, phase);
+	}
    
-   //solving with KRAMER
-   //matrica 2x2
-   public Solver(double a11, double a12, double a13, double b11, double b12, double b13)
-   {
-	   this.delta = this.percaktorSolver(a11, a12, b11, b12);
-	   this.x2 = this.percaktorSolver(a11, a13, b11, b13)/this.delta;
-	   this.x1 = this.percaktorSolver(a13, a12, b13, b12)/this.delta;
-	   
-   }
    
-   //matrica 3x3
-   public Solver(double a11, double a12, double a13, double a14, double b11, double b12, double b13, double b14, double c11, double c12, double c13, double c14)
-   {
-	   this.delta = a11*this.percaktorSolver(b12, b13, c12, c13) - b11*this.percaktorSolver(a12, a13, c12, c13) + 
-			   c11*this.percaktorSolver(a12, a13, b12, b13);
-	   
-	   this.x1 = (a14*this.percaktorSolver(b12, b13, c12, c13) - b14*this.percaktorSolver(a12, a13, c12, c13) + 
-			   c14*this.percaktorSolver(a12, a13, b12, b13))/this.delta;
-	   
-	   this.x2 = (a11*this.percaktorSolver(b14, b13, c13, c13) - b11*this.percaktorSolver(a14, a13, c14, c13) + 
-			   c11*this.percaktorSolver(a14, a13, b14, b13))/this.delta;
-	   
-	   this.x3 = (a11*this.percaktorSolver(b12, b14, c12, c14) - b11*this.percaktorSolver(a12, a14, c12, c14) + 
-			   c11*this.percaktorSolver(a12, a14, b12, b14))/this.delta;
-   }
-   
-    //2x2 percaktor solver
- 	public double percaktorSolver(double a11, double a12, double b11, double b12)
- 	{
- 		return a11*b12 - a12*b11;
- 	}
 }
 
-class Debbuger{
-	
-	Solver solver = new Solver(1, -1, 1, 1, 1, 3);
-	
-	
-}
+   
+
+
