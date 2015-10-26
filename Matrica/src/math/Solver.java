8848df1da3a17d4a7745b2275bd4eb3b90125214
Mateo Jovani <mateojovani@yahoo.com>
2015-10-26 14:36:17 +0100

    completed 2x2 solver

--- Matrica/src/math/Solver.java
+++ Matrica/src/math/Solver.java
@@ -2,8 +2,38 @@ package math;
 
 public class Solver
 {
-   public Solver(int a11, int a12, int b11, int b12)
+	private double delta;
+	private double x1;
+	private double x2;
+	
+	public double getX1()
+	{
+		return this.x1;
+	}
+	
+	public double getX2()
+	{
+		return this.x2;
+	}
+   //matrica 2x2
+   public Solver(double a11, double a12, double a13, double b11, double b12, double b13)
+   {
+	   this.delta = a11*b12 - a12*b11;
+	   this.x2 = (a13*b12 - a12*b13)/this.delta;
+	   this.x1 = (a11*b13 - a13*b11)/this.delta;
+	   
+   }
+   
+   //matrica 3x3
+   public Solver(double a11, double a12, double a13, double a14, double b11, double b12, double b13, double b14, double c11, double c12, double c13, double c14)
    {
 	   
    }
+}
+
+class Debbuger{
+	
+	Solver solver = new Solver(1, -1, 1, 1, 1, 3);
+	
+	
 }
