a9df1c0d335f7f08532599004fa69550bf84b683
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-10-28 00:43:11 +0100

    added tests and restructured the Matrix class

--- src/main/java/matrix/math/Matrix.java
+++ src/main/java/matrix/math/Matrix.java
@@ -1,39 +1,98 @@
 package matrix.math;
 
+/**
+ * The {@code Matrix} class creates a Matrix instance from a 2D array
+ * and also holds methods for manipulating the Matrix.
+ */
 public class Matrix {
+    //TODO: Defensive programming: Matrix should be square
+
     //TODO: Remove these two lines. Create matrix as array of arrays
     public boolean type; //c'lloj matrice jam? 0-2x2||1-3x3/
     public double a1, a2, a3, a4, b1, b2, b3, b4, c1, c2, c3, c4;
     private double[][] matrix;
 
+    /**
+     * Protected empty constructor to not permit the creation of
+     * empty matrices
+     */
+    protected Matrix() {
+    }
+
+    /**
+     * Constructor to create a {@code Matrix} instance from a 2D array
+     *
+     * @param matrix The 2D array, which should be a square array
+     * @author Aldo Ziflaj
+     */
     public Matrix(double[][] matrix) {
         this.matrix = matrix;
     }
 
-//    public Matrix(double a1, double a2, double a3, double a4, double b1, double b2, double b3, double b4, double c1, double c2, double c3, double c4) {
-//        this.a1 = a1;
-//        this.a2 = a2;
-//        this.a3 = a3;
-//        this.a4 = a4;
-//        this.b1 = b1;
-//        this.b2 = b2;
-//        this.b3 = b3;
-//        this.b4 = b4;
-//        this.c1 = c1;
-//        this.c2 = c2;
-//        this.c3 = c3;
-//        this.c4 = c4;
-//        this.type = true;
-//    }
-
-    public void printValues() {
-        if (!this.type) {
-            System.out.println(this.a1 + " " + this.a2 + " | " + this.a3);
-            System.out.println(this.b1 + " " + this.b2 + " | " + this.b3);
+    /**
+     * Stringifies the value of the matrix and prints it for logging purposes
+     *
+     * @return The stringified value of the matrix
+     */
+    public String printValues() {
+        StringBuilder sb = new StringBuilder();
+        for (double[] arr : matrix) {
+            for (double item : arr) {
+                sb.append(item).append(" ");
+            }
+            sb.append("\n");
         }
+
+        System.out.println(sb.toString());
+
+        return sb.toString();
     }
 
+    /**
+     * Returns the Matrix item at a given position
+     *
+     * @param row The row of the item to get
+     * @param col The column of the item to get
+     * @return The item at the given position
+     * @author Aldo Ziflaj
+     */
     public double getItemAtPosition(int row, int col) {
         return matrix[row][col];
+    }
+
+    public double[][] getMatrixAsArray() {
+        return matrix;
+    }
+
+    /**
+     * Returns the minor of the matrix for a given row and columnt
+     *
+     * @param row The given row for the minor matrix
+     * @param col The given column for the minor matrix
+     * @return The minor matrix
+     * @author Aldo Ziflaj
+     */
+    public Matrix getMinor(int row, int col) {
+        int dim = matrix[0].length;
+        int nDim = dim - 1;
+        double[][] minor = new double[nDim][nDim];
+
+        int a = 0, b = 0;
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                if (i != row && j != col) { //elem. of minor
+                    minor[a][b] = matrix[i][j];
+                    b++;
+                    if (b == nDim) {
+                        b = 0;
+                    }
+                }
+            }
+            if (i != row) {
+                a++;
+            }
+        }
+
+        return new Matrix(minor);
     }
 }
--- src/test/java/matrix/math/MatrixTest.java
+++ src/test/java/matrix/math/MatrixTest.java
@@ -4,17 +4,36 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
+
+/**
+ * Test class for {@code matrix.math.Matrix} class
+ */
 public class MatrixTest {
     Matrix matrix;
+    double[][] _matrix;
 
     @Before
     public void init() {
-        double[][] _matrix = new double[][] {{1, 2, 3}, {2, 1, 3}, {3, 2, 1}};
+        _matrix = new double[][] {{1, 2, 3}, {2, 1, 3}, {3, 2, 1}};
         matrix = new Matrix(_matrix);
     }
 
     @Test
     public void testItemAtPosition() {
         Assert.assertEquals("Items should match!", 2, matrix.getItemAtPosition(0, 1), 0);
+    }
+
+    @Test
+    public void testMatrixAsArray() {
+        //TODO: Method is deprecated; change
+        Assert.assertEquals("Matrix and array don't match", _matrix, matrix.getMatrixAsArray());
+    }
+
+    @Test
+    public void testMinorOfMatrix() {
+        Matrix minorMatrix = matrix.getMinor(0, 0);
+        double[][] _minor = new double[][] {{1, 3}, {2, 1}};
+        //TODO: Method is deprecated; change
+        Assert.assertEquals("Matrix and array don't match: " + minorMatrix.printValues(), _minor, minorMatrix.getMatrixAsArray());
     }
 }
